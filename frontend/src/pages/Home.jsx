b3442ee4fe9c385172125e05d7da76cee8b408fd
Premkmaurya <premkmaurya123456@gmail.com>
2025-11-23 12:38:04 +0530

    feat: Add Footer component and update Home layout with responsive design

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import Nav from '../components/home/hero/Nav'
-import BottomRight from '../components/home/hero/BottomRight'
-import BottomLeft from '../components/home/hero/BottomLeft'
-import BottomCenter from '../components/home/hero/BottomCenter'
+import React from "react";
+import Nav from "../components/home/hero/Nav";
+import BottomRight from "../components/home/hero/BottomRight";
+import BottomLeft from "../components/home/hero/BottomLeft";
+import BottomCenter from "../components/home/hero/BottomCenter";
+import Footer from "../components/home/footer/Footer";
 
 const Home = () => {
   return (
-    <div className='relative w-screen h-screen'>
-      <div className='absolute top-0 left-0 w-full h-full'>
-        <img className='w-full h-full bg-cover' src="/img/group-study.jpg" alt="" />
+    <div className="overflow-x-hidden">
+      <div className="relative w-screen h-screen">
+        <div className="absolute top-0 left-0 w-full h-full">
+          <img
+            className="w-full h-full bg-cover"
+            src="/img/group-study.jpg"
+            alt=""
+          />
+        </div>
+        <Nav />
+        <div className="w-screen h-[60vh] rounded-b-4xl bg-transparent overflow-hidden"></div>
+        <BottomLeft />
+        <BottomCenter />
+        <BottomRight />
       </div>
-      <Nav />
-      <div className='w-screen h-[60vh] rounded-b-4xl bg-transparent overflow-hidden'></div>
-      <BottomLeft />
-      <BottomCenter />
-      <BottomRight />
+      <div className="w-screen overflow-x-hidden">
+        <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
